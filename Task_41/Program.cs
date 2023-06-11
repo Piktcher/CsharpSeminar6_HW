a9e31ba63ff9c35c68324dce8635eee812e1347d
Pavel Ordenov <pavelordenov@yandex.ru>
2023-06-11 21:57:33 +0200

    Completed tasks

---
+++ Task_41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int CountPositiveNumbers (int amount)
+{
+    int positiveNumbers = 0;
+    int count = 1;
+    while (count <= amount)
+    {
+        Console.Write($"Введите число номер {count} : ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        count++;
+        if (number > 0) positiveNumbers++;
+    }
+
+    return positiveNumbers;
+}
+
+Console.Write($"Введите кол-во чисел, среди которых нужно посчитать положительные - ");
+int quantity = Convert.ToInt32(Console.ReadLine());
+
+int result = CountPositiveNumbers(quantity);
+Console.WriteLine($"Количество положительных чисел среди введенных равно {result}");
---
+++ Task_43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double FindIntersectionPointHorizontalAxis (double k1, double b1, double k2, double b2)
+{
+    double horizontalAxis = (b2 - b1) / (k1 - k2);
+    return horizontalAxis;
+}
+
+double FindIntersectionPointVerticalAxis (double k1, double b1, double horizontalAxis)
+{
+    double verticalAxis = k1 * horizontalAxis + b1;
+    return verticalAxis;
+}
+
+Console.WriteLine("Введите необходимые коэффициенты для 2-ух линейных уравнений");
+Console.Write("k1 = ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b1 = ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k2 = ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b2 = ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double resultX = FindIntersectionPointHorizontalAxis(k1, b1, k2, b2);
+double resultY = FindIntersectionPointVerticalAxis(k1, b1, FindIntersectionPointHorizontalAxis(k1, b1, k2, b2));
+
+Console.WriteLine($"Точка пересечения двух уравнений на графике - ({resultX};{resultY})");
